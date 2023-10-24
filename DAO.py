6689ca2bcacee3fc99670df884650c065a659730
1GabsFps <gabriel.neco@live.com>
2023-10-24 12:53:52 -0300

    update

--- DAO.py
+++ DAO.py
@@ -1,9 +1,16 @@
+import random
+
+
 def adicionar_tarefa(tarefa):
     with open("To-do.txt", "r") as arquivo:
         tarefas = arquivo.readlines()
         indice = len(tarefas)
-    with open("To-do.txt", "a") as arquivo:
-        arquivo.write(f"\n {indice}    {tarefa}\n")
+        id = random.randint(1000, 9999)
+        if id in tarefas:
+            id = random.randint(1000, 9999)
+        else:
+            with open("To-do.txt", "a") as arquivo:
+                arquivo.write(f"\n {id}    {tarefa}")
 
 
 def listar_tarefas():
